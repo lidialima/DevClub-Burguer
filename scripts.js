7fdf99293d78a3b5e05f731f37120e703b7e084e
lidialima <lidialima2lima@gmail.com>
2024-09-28 18:09:17 -0300

    JavaScript pt.4 - Front End Club

---
+++ scripts.js
@@ -0,0 +1,65 @@
+const list = document.querySelector('ul')
+const buttonShowAll = document.querySelector('.show-all')
+const buttonMapAll = document.querySelector('.map-all')
+const sumAll = document.querySelector('.sum-all')
+const filterAll = document.querySelector('.filer-all')
+
+
+function formaCurrency(value) {
+  const newValue = value.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+
+  return newValue
+}
+
+
+function showAll(productsArray) {
+  let myLi = ""
+
+  productsArray.forEach((product) => {
+    myLi += `
+        
+           <li>
+                <img src="${product.src}">
+                <p>${product.name}</p>
+                <p class="item-price">${formaCurrency(product.price)}</p>
+            </li>
+        
+        `
+  });
+
+  list.innerHTML = myLi
+}
+
+
+function mapAllItems (){
+ const newPrices = menuOptions.map((product) => ({
+  ...product,
+  price: product.price * 0.9, // Dae 10% de desconto
+
+ }))
+
+ showAll(newPrices)
+
+}
+
+function sumAllItems(){
+  const totalValue = menuOptions.reduce((acc, curr) => acc + curr.price, 0)
+  list.innerHTML = `
+  <li>
+  <p>O valor total dos itens é R$ ${formaCurrency(totalValue)} </p>
+  </li>
+  `
+
+  console.log(totalValue)
+}
+
+function filterAllItems(){
+  const filterJustVegan = menuOptions.filter( (product) => product.vegan)
+
+  showAll(filterJustVegan)
+}
+
+buttonShowAll.addEventListener('click', () => showAll(menuOptions))
+buttonMapAll.addEventListener('click', mapAllItems)
+sumAll.addEventListener('click', sumAllItems)
+filterAll.addEventListener('click', filterAllItems)
